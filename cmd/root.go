ea872a3a72a28f6a98015fa2b85df16116202128
KeisukeYamashita <19yamashita15@gmail.com>
2021-05-13 02:09:27 +0900

    Add --version flag
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -3,12 +3,14 @@ package cmd
 import (
 	"github.com/kutifyhq/autifyctl/cmd/schedule"
 	"github.com/kutifyhq/autifyctl/pkg/option"
+	"github.com/kutifyhq/autifyctl/pkg/version"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "autifyctl",
-	Short: "CLI tool for Autify API",
+	Use:     "autifyctl",
+	Short:   "CLI tool for Autify API",
+	Version: version.String(),
 }
 
 func init() {
